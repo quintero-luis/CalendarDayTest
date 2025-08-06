0b96f60585809204101189e1bcb3886bc6aec017
Quinterock <luisgqr1975@gmail.com>
2025-08-06 12:27:36 -0600

    First commit

---
+++ CalendarDay/CalendarDayApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalendarDayApp.swift
+//  CalendarDay
+//
+//  Created by Luis Quintero on 06/08/25.
+//
+
+import SwiftUI
+
+@main
+struct CalendarDayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CalendarDayTests/CalendarDayTests.swift
@@ -0,0 +1,17 @@
+//
+//  CalendarDayTests.swift
+//  CalendarDayTests
+//
+//  Created by Luis Quintero on 06/08/25.
+//
+
+import Testing
+@testable import CalendarDay
+
+struct CalendarDayTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}

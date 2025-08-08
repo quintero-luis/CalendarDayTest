bdb9ab103d6c0987a52169906e1c76e90f58ca06
Quinterock <luisgqr1975@gmail.com>
2025-08-07 19:10:34 -0600

    Better ContentView

--- CalendarDay/Views/ContentView.swift
+++ CalendarDay/Views/ContentView.swift
@@ -18,27 +18,52 @@ struct ContentView: View {
     
     var body: some View {
         VStack {
-            Button("Generate Current Week") {
-                viewModel.generateCurrentWeek(context: context)
-            }
             List(days) { day in
                 VStack(alignment: .leading) {
-                    Text(day.date.formatted(date: .abbreviated, time: .omitted))
-                    if !day.dayNote.isEmpty {
-                        Text(day.dayNote)
-                            .font(.caption)
+                    HStack {
+                        Text(day.date.formatted(.dateTime.weekday())) // Day of the week
+                            .font(.title2)
+                            .fontWeight(.semibold)
+                            .frame(alignment: .topTrailing)
+                            .padding(.top, -32)
+                        Spacer()
+                        Text(day.date.formatted(date: .abbreviated, time: .omitted)) // Date
                             .foregroundColor(.gray)
-                    }
-                }
+                        
+                    } // HStack
+                    .padding(.vertical, 8)
+                    
+                    if !day.dayNote.isEmpty {
+                                Text(day.dayNote)
+                            .font(.body)
+                                    
+                            } else {
+                                Text("Empty Note")
+                                    .font(.subheadline)
+                                    .italic()
+                                    .foregroundColor(.gray)
+                            }
+                } // 2nd VStack
+                .padding()
+                .frame(minWidth: 372)
+                .frame(maxWidth: .infinity)
+                .frame(minHeight: 140)
+                .background(Color(.systemBackground))
+                .cornerRadius(12)
+                .shadow(color: .black.opacity(0.2), radius: 4, x: 0, y: 4)
+                .padding(.horizontal, 0)
                 .onTapGesture {
                     selectedDay = day
                 }
-            }
-        }
+                .listRowSeparator(.hidden) // Hides default lines
+            } // List
+            .listStyle(.plain)
+            .scrollContentBackground(.hidden)
+            .padding(.top, 8)
+        } // VStack
         .sheet(item: $selectedDay, content: { day in
             EditNoteView(day: day)
         })
-        .padding()
     }
 }
 
